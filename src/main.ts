4b279a7cb0e131408b1fe32310b3e86738404c64
Arush Bansal <ch1210371@chemical.iitd.ac.in>
2025-06-17 09:23:44 +0530

    deployment

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
-
-const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-  await app.init();
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 8080);
 }
-
 void bootstrap();
-
-export default server;
